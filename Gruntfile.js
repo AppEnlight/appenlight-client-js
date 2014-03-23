69bec560208fa6dab83254d0072e24f7b497602c
Marcin Lulek <info@webreactor.eu>
2014-03-23 23:58:10 +0100

    added grunt files and reorganized things

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: '\n'
+      },
+      dist: {
+        src: [
+        "src/appenlight-client.js",
+    ],
+        dest: 'appenlight-client.js'
+      },
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    watch: {
+      files: ['<%= concat.dist.src %>'],
+      tasks: ['concat']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', ['jshint', 'qunit']);
+
+  grunt.registerTask('default', ['concat']);
+
+};
